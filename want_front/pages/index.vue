6bb6469386c4a9cf610b8735bab0a251a327c6a1
tryoma <tryoma0326@gmail.com>
2022-01-18 14:12:40 +0900

    first-commit

---
+++ want_front/pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <AddTodo @submit="addTodo" />
+    <TodoList :todos="todos" />
+  </div>
+</template>
+
+<script>
+  import AddTodo from "@/components/AddTodo";
+  import TodoList from "@/components/TodoList";
+  import axios from "@/plugins/axios";
+
+  export default {
+    components: {
+      AddTodo,
+      TodoList,
+    },
+    data() {
+      return {
+        todos: [],
+      };
+    },
+    methods: {
+      async addTodo(title) {
+        await axios.post("/v1/users", { title });
+        this.todos.push({
+          title
+        });
+      },
+    },
+  };
+
+</script>
+
+<style>
+</style>
